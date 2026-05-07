1786fee948bda35a3638be0b1eb359ebfb5008de
devansh-codes614 <devanshtripathi614@gmail.com>
2026-05-07 10:44:31 +0530

    redeploy

--- Frontend/src/apiClient.js
+++ Frontend/src/apiClient.js
@@ -1,11 +1,12 @@
-// Frontend/src/apiClient.js
+// Frontend/src/apiclient.js
+
 import axios from "axios";
 
-console.log("Using API base URL => https://sentramind-backend.onrender.com");
+console.log("Using API base URL => https://sentramind-backend-ymzz.onrender.com");
 
 const api = axios.create({
-  baseURL: "https://sentramind-backend.onrender.com", // backend root
-  withCredentials: true,            // sessions/cookies for login
+  baseURL: "https://sentramind-backend-ymzz.onrender.com",
+  withCredentials: true,
 });
 
 export default api;
