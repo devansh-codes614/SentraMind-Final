dc14f445cfa0d5458ce12765b5553a0486d4b5e5
devansh-codes614 <devanshtripathi614@gmail.com>
2026-05-07 10:27:22 +0530

    Update backend URL

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true }));
 // =====================
 const allowedOrigins = [
   "http://localhost:5173",
-  "http://https://sentramind-final.onrender.com:5173",
+  "http://https://https://sentramind-backend-ymzz.onrender.com:5173",
 ];
 
 app.use(cors({
@@ -137,7 +137,7 @@ app.use((err, req, res, next) => {
 // MongoDB Connection
 // =====================
 const dbUrl =
-  process.env.MONGODB_URI || "mongodb://https://sentramind-final.onrender.com:27017/sentramind";
+  process.env.MONGODB_URI || "mongodb://https://https://sentramind-backend-ymzz.onrender.com:27017/sentramind";
 
 async function connectDB() {
   try {
